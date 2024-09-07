7ab90fa62d90061303a3fb363ee6d6123e041223
carlos <carlos.ferduardo@gmail.com>
2024-09-07 15:45:54 -0400

    se termina conexion

---
+++ conexion.php
@@ -0,0 +1,30 @@
+<?php 
+    class Conexion{
+        private $servidor = "localhost";
+        private $usuario = "root";
+        private $contraseña = "";
+        private $conexion;
+        
+        public function __construct(){
+            try{
+                $this->conexion = new PDO("mysql:host=$this->servidor;dbname=mascotiamigos", $this->usuario, $this->contraseña);
+                $this->conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e) {
+                return "falle de conexión".$e;
+            }
+        }
+
+        public function ejecutar($sql){     //Insertar/Delete/Actualizar
+            $this->conexion->exec($sql);
+            return $this->conexion->lastInsertId();
+        }
+
+        public function consultar($sql){
+            $sentencia = $this->conexion->prepare($sql);
+            $sentencia->execute();
+            return $sentencia->fetchAll();      //fetchAll() = Retornar todos los registros que puedas consulgar con la sentencia sql
+        }
+
+
+    }
+?>
