91a8acb2fa7fe15b046aaf14c9ded97f2fd20e09
carlos <carlos.ferduardo@gmail.com>
2024-09-08 13:32:43 -0400

    se establece el nav global

--- cabecera.php
+++ cabecera.php
@@ -5,14 +5,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
     <title>Mascotiamigos</title>
-    <link rel="sohrtcut icon" href="./img/IMG-20240905-WA0018.jpg">
+    <link rel="icon" href="./img/logo.jpg" type="image/jpeg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">                 <!--bs5-grid-container = contenedor grid con boostrap--> 
-        <a href="index.php">Inicio</a> |
-        <a href="historial_clinico.php">Historial Clínico</a> |
-        <a href="cerrar.php">Cerrar</a>
-        <br/>
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="./index.php">Inicio</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="./historial_clinico.php">Servicios</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./login_pets.php">Registrarse</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
