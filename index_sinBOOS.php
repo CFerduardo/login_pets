db14e9a7eea01102142ab978e38be905d4a5028f
carlos <carlos.ferduardo@gmail.com>
2024-09-08 13:34:01 -0400

    se empiezan estilos con boostrap

---
+++ index_sinBOOS.php
@@ -0,0 +1,30 @@
+<?php include "cabecera.php"      //include = llamar a ese fragmento de código "nombre del archivo"?>
+
+<!--navegacion
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container px-5">
+        <a class="navbar-brand" href="#page-top">Bienvenidos 🐾</a> 
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <!--<div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#!">Servicios</a></li>
+                <li class="nav-item"><a class="nav-link" href="#!">Log In</a></li>
+            </ul> 
+        </div>
+    </div>
+</nav>-->
+
+
+
+
+
+<div class="p-5 mb-4 bg-light rounded-3" >                   <!--bs5-jumbotron-default = contenedor de gran tamaño que se utiliza para resaltar contenido importante en una página web. Por lo general, se utiliza para mostrar un título grande, un subtítulo y un llamado a la acción ////         <div class="h-100 p-5 text-white bg-primary border rounded-5"> clase anterior con el cuadro azzul-->
+    <div class="container-fluid py-5"">
+            <h1 class="display-5 fw-bold">Bienvenidos 🐾</h1>
+            <p class="col-md-8 fs-4">´La tienda de las colitas felices´</p>
+            <hr class="my-2">
+            <p>Más Información</p>
+    </div>
+</div>
+
+<?php include "pie.php"?>
