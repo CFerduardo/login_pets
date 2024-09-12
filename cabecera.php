9403b8a36ecbb7dcf6dfc4cfedabeebc21c4df2b
carlos <carlos.ferduardo@gmail.com>
2024-09-12 13:07:23 -0400

    se estructura mejor

--- cabecera.php
+++ cabecera.php
@@ -33,3 +33,17 @@
                 </div>
             </div>
         </nav>
+        <!-- Header-->
+        <header class="masthead text-center text-white">
+            <div class="masthead-content">
+                <div class="container px-4">
+                    <h1 class="masthead-heading mb-0"><i>Mascotiamigos Enterprise🐾</i></h1>
+                    <h2 class="masthead-subheading mb-0"><i>La tienda de las colitas felices</i></h2>
+                    <!--<a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Conoce más</a>-->
+                </div>
+            </div>
+            <div class="bg-circle-1 bg-circle"></div>
+            <div class="bg-circle-2 bg-circle"></div>
+            <div class="bg-circle-3 bg-circle"></div>
+            <div class="bg-circle-4 bg-circle"></div>
+        </header>
