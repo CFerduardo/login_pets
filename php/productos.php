8316005bd3b23a7543d689668e03a318f577e91c
carlos <carlos.ferduardo@gmail.com>
2024-09-16 16:11:54 -0400

    se inrtegra el carrusel

--- php/productos.php
+++ php/productos.php
@@ -1,3 +1,48 @@
 <?php include "cabecera.php"?>
-
+<div class="slider">
+        <div class="slider-track">
+            <div class="slide">
+                <img src="../img/111100174-v1-min.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/14401-7704269225091.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/D_NQ_NP_2X_663594-MLV44750527250_012021-E.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/unnamed.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/unnamed.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/Z_16_883b040e-cc11-4176-b6c5-2a2dcd024632.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/13900-Comida-para-perro-Poder-Canino-02.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/111100174-v1-min.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/14401-7704269225091.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/D_NQ_NP_2X_663594-MLV44750527250_012021-E.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/unnamed.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/unnamed.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/Z_16_883b040e-cc11-4176-b6c5-2a2dcd024632.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/13900-Comida-para-perro-Poder-Canino-02.jpg" alt="">
+            </div>
+        </div>
+    </div>
 <?php include "pie.php"?>
