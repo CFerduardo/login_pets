3c04a8182bad931a679e599787bdffe22f4a105a
carlos <carlos.ferduardo@gmail.com>
2024-09-16 23:44:16 -0400

    se incluye lorem

--- php/productos.php
+++ php/productos.php
@@ -14,6 +14,18 @@
                 <img src="../img/unnamed.jpg" alt="">
             </div>
             <div class="slide">
+                <img src="../img/111100174-v1-min.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/14401-7704269225091.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/D_NQ_NP_2X_663594-MLV44750527250_012021-E.webp" alt="">
+            </div>
+            <div class="slide">
+                <img src="../img/unnamed.jpg" alt="">
+            </div>
+            <div class="slide">
                 <img src="../img/unnamed.webp" alt="">
             </div>
             <div class="slide">
@@ -45,4 +57,56 @@
             </div>
         </div>
     </div>
+
+
+</br>
+        <!-- Content section 1-->
+        <section id="scroll">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="../img/perfilPerro.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">¡Te saludamos!</h2>
+                            <p>Lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. A laborum eius, unde at magnam voluptatem in totam quidem sint molestiae soluta, porro voluptates placeat temporibus dolor aliquam pariatur facilis iusto!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ullam nam accusantium odit aperiam debitis et eos numquam perferendis, distinctio facere quod esse? Laborum neque sequi qui alias! Quo, voluptatem.ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 2-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="../img/perfilPerro.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h2 class="display-4">We salute you!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content section 3-->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="../img/perfilPerro.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">Let there be rock!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </section>
+
 <?php include "pie.php"?>
